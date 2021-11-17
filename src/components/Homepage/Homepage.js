3f628413becbe1b0f6ffd1550daf5f121a7fa662
jeannette.valencia <jfatima.valencia@gmail.com>
2021-11-17 19:43:00 +0100

    Homepage Upgrade

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -11,6 +11,9 @@ class Homepage extends Component {
         <h4>iCollect</h4>
         <h6>an App to keep track of your favourite collections!</h6>
         <img src="/homepage.jpeg" alt="background"/>
+
+        {/*<div className="homepage">
+        <img src='/homepage.jpeg' height={'750px'} width={'100%'} alt="good-coffee-mood"/>*/}
       </div>
     )
 
