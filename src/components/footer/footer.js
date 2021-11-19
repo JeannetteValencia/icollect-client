47bf555df1046503b91ee7bbbbc2a8b1b5027796
jeannette.valencia <jfatima.valencia@gmail.com>
2021-11-19 13:49:10 +0100

    Creating the footer component and its links

---
+++ src/components/footer/footer.js
@@ -0,0 +1,19 @@
+// components/navbar/Navbar.js
+
+import React, { Component } from 'react';
+
+
+class Footer extends Component {
+
+  render() {
+      return (
+        <div className="footer">
+          <a href="https://github.com/JeannetteValencia">Follow me in GitHub</a>
+          <img src="/logo.png" alt="icollect" height='{2rem}'/>
+          <a href="https://profiles.ironhack.com/jeannette-valencia">About Me</a>
+        </div>
+      );
+  }
+}
+
+export default Footer;
